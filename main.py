016f8a0387770d71f0df9d81a29c3fc014782004
hellicopthecat <114468016+hellicopthecat@users.noreply.github.com>
2023-02-07 11:47:09 +0900

    install requests by pypi

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-# for loop
+# requests
+
+from requests import get
+# get은 function, 이동한 다음에 말 그대로 website를 가져오는것
 
 websites=(
     "google.com",
     "airbnb.com",
-    "twitter.com",
+    "https://twitter.com",
     "facebook.com",
-    "instagram.com"
+    "https://instagram.com"
 )
 
-for each in websites:
-    print("hello", each)
+# for website in websites:
+#     if website.startswith("https://"):
+#         print("good to go")
+#     else:
+#         print("we have to fix it")
+
+# for website in websites:
+#     if not website.startswith("https://"):
+#         print("we have to fix it")
+#     else:
+#         print("good to go")
+
+for website in websites:
+    if not website.startswith("https://"):
+        website = f"https://{website}"
+    print(website)
